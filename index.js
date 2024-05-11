3295f14631eafeb9669281fa6993df7819b4fb26
HaritaChippa19 <95666498+HaritaChippa19@users.noreply.github.com>
2024-05-11 10:58:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import ContextProvider from './context/Context.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
+
+reportWebVitals();
